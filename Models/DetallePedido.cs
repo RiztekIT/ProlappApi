fc349ce3fec2e913cc6d6fbcb8705fd8ba94c9e1
javier sierra <javier.sierra@riztek.com.mx>
2020-01-08 15:05:54 -0700

    Modelos y controladores de PagoCFDI, Metodo de Pago

---
+++ Models/DetallePedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProlappApi.Models
+{
+    public class DetallePedido
+    {
+
+        public long IdDetallePedido { get; set; }
+        public long IdPedido { get; set; }
+        public string ClaveProducto { get; set; }
+        public string Producto { get; set; }
+        public string Unidad { get; set; }
+        public string ClaveSat { get; set; }
+        public string PrecioUnitario { get; set; }
+        public string Cantidad { get; set; }
+        public string Importe { get; set; }
+        public string Observaciones { get; set; }
+        public string TextoExtra { get; set; }
+
+    }
+}
