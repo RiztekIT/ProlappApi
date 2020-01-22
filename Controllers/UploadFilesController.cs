2c87d4aa19f47f09da04d5a16a4676e70497d10e
RiztekTI <riztekti@gmail.com>
2020-01-22 15:56:44 -0700

    .

--- Controllers/UploadFilesController.cs
+++ Controllers/UploadFilesController.cs
@@ -17,7 +17,7 @@ namespace ProlappApi.Controllers
             int iUploadedCnt = 0;
 
             string sPath = "";
-            sPath = System.Web.Hosting.HostingEnvironment.MapPath("~/FileTemps/");
+            sPath = System.Web.Hosting.HostingEnvironment.MapPath("C:/FileTemps/");
 
             System.Web.HttpFileCollection hfc = System.Web.HttpContext.Current.Request.Files;
 
