e66441e761e4892889e7496524eeae9d855fde99
RiztekIT <javier.gutierrez@riztek.com.mx>
2020-01-22 12:59:18 -0700

    Controlador FileUploader

---
+++ Controllers/UploadFilesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+
+
+namespace ProlappApi.Controllers
+{
+    public class UploadFilesController : ApiController
+    {
+        [HttpPost()]
+        public string UploadFiles()
+        {
+            int iUploadedCnt = 0;
+
+            string sPath = "";
+            sPath = System.Web.Hosting.HostingEnvironment.MapPath("~/FileTemps/");
+
+            System.Web.HttpFileCollection hfc = System.Web.HttpContext.Current.Request.Files;
+
+
+            for (int iCnt = 0; iCnt <= hfc.Count - 1; iCnt++)
+            {
+                System.Web.HttpPostedFile hpf = hfc[iCnt];
+
+                if (hpf.ContentLength > 0)
+                {
+
+                    if (!File.Exists(sPath + Path.GetFileName(hpf.FileName)))
+                    {
+                        hpf.SaveAs(sPath + Path.GetFileName(hpf.FileName));
+                        iUploadedCnt = iUploadedCnt + 1;
+                    }
+                }
+            }
+
+            if (iUploadedCnt > 0)
+            {
+                return iUploadedCnt + " Files Uploaded Successfully";
+            }
+            else
+            {
+                return "Upload Failed";
+            }
+        }
+
+    }
+}
