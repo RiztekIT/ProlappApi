315fdd1af24c55d422c2e087db0ebb02cb7c6fd5
RiztekIT <javier.gutierrez@riztek.com.mx>
2020-01-30 12:14:08 -0700

    Controladores

--- Controllers/VendedorController.cs
+++ Controllers/VendedorController.cs
@@ -5,10 +5,31 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+
+using ProlappApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
 namespace ProlappApi.Controllers
 {
     public class VendedorController : ApiController
     {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
 
+            string query = @"select * from Vendedor";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
     }
 }
