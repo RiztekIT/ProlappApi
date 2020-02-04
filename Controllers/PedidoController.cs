4c32961b5fdd1d28ba3e6a47e8b8b3310bded9a3
javier sierra <javier.sierra@riztek.com.mx>
2020-02-04 13:08:55 -0700

    Update / Delete Pedido Controller

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -55,7 +55,7 @@ namespace ProlappApi.Controllers
                                 + time2.ToString(format) + "' , '" + pedido.OrdenDeCompra + "' , '"
                                 + time3.ToString(format) + "' , '" + pedido.CondicionesDePago + "' , '" + pedido.Vendedor + "' , '"
                                 + pedido.Estatus + "' , '" + pedido.Usuario + "' , '"
-                                + pedido.Factura + "' , '" + pedido.LugarDeEntrega + "' , '" + pedido.Moneda + @"'
+                                + pedido.Factura + "' , '" + pedido.LugarDeEntrega + "' , '" + pedido.Moneda + "' , '" + pedido.Prioridad + @"'
                                 ";
 
                 using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
@@ -73,6 +73,78 @@ namespace ProlappApi.Controllers
             catch (Exception exe)
             {
                 return "Se produjo un error" + exe;
+            }
+        }
+
+        public string Put(Pedido pedido)
+        {
+            try
+            {
+
+
+                DataTable table = new DataTable();
+                //Las variables de fecha, son igualadas a un valor Datatime
+                DateTime time2 = pedido.FechaVencimiento;
+                DateTime time3 = pedido.FechaDeEntrega;
+                //Al momento de insertar los valores de las fechas, estan seran insertadas con el formato 'Format'
+                string format = "yyyy-MM-dd HH:mm:ss";
+                //De esta manera no causara error al tratar de insertar fechas en la base de datos SQL
+                //time.ToString(format)
+                string query = @"
+                                Execute etEditarPedido " + pedido.IdPedido + " , "+ pedido.IdCliente + " , '" + pedido.Folio + "' , '"
+                                + pedido.Subtotal + "' , '" + pedido.Descuento + "' , '"
+                                + pedido.Total + "' , '" + pedido.Observaciones + "' , '"
+                                + time2.ToString(format) + "' , '" + pedido.OrdenDeCompra + "' , '"
+                                + time3.ToString(format) + "' , '" + pedido.CondicionesDePago + "' , '" + pedido.Vendedor + "' , '"
+                                + pedido.Estatus + "' , '" + pedido.Usuario + "' , '"
+                                + pedido.Factura + "' , '" + pedido.LugarDeEntrega + "' , '" + pedido.Moneda + "' , '" + pedido.Prioridad + @"'
+                                ";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+
+
+                return "Pedido Actualizado";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+
+
+                DataTable table = new DataTable();
+
+
+                string query = @"
+                              exec dtBorrarPedido " + id;
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+
+
+                return "Se Elimino Correctamente";
+            }
+            catch (Exception)
+            {
+                return "Se produjo un error";
             }
         }
 
