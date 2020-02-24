8c49c665ec4715238b5c2d9243c80b637e21dfd0
sierra_javier <javier.sierra@riztek.com.mx>
2020-02-24 10:20:14 -0700

    Unidad Medida controler/model

---
+++ Controllers/UnidadMedidaController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ProlappApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace ProlappApi.Controllers
+{
+    [RoutePrefix("api/UnidadMedida")]
+    public class UnidadMedidaController : ApiController
+    {
+
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            string query = @"Select * from UnidadMedida";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+
+
+        public string Post(UnidadMedida um)
+        {
+            try
+            {
+
+
+                DataTable table = new DataTable();
+                string query = @"
+                              insert into UnidadMedida ( Nombre, ClaveSAT) values (" + "'" + um.Nombre + "','" + um.ClaveSAT + "');";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+
+
+                return "Unidad de Medida Agregada";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+
+
+                DataTable table = new DataTable();
+
+
+                string query = @"
+                              Delete UnidadMedida where IdUnidadMedida = " + id;
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+
+
+                return "Se Elimino Correctamente";
+            }
+            catch (Exception)
+            {
+                return "Se produjo un error";
+            }
+        }
+
+        public string Put(UnidadMedida um)
+        {
+            try
+            {
+
+
+                DataTable table = new DataTable();
+
+                string query = @"
+                                update UnidadMedida set Nombre = '" + um.Nombre + "','" + um.ClaveSAT + "' where IdUnidadMedida = " + um.IdUnidadMedida + ";";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+
+
+                return "Se Actualizo Correctamente";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+
+
+
+
+
+
+
+            }
+        }
+
+
+
+
+    }
+}
