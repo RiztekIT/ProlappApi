9efae47fc6186b49f5f56e075a8ae330ef74369d
sierra_javier <javier.sierra@riztek.com.mx>
2020-02-26 14:52:04 -0700

    Direccion Cliente Model/Controler

---
+++ Controllers/ClienteDireccionController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ProlappApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProlappApi.Controllers
+{
+    [RoutePrefix("api/ClienteDireccion")]
+    public class ClienteDireccionController : ApiController
+    {
+
+
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            string query = @"Select * from DireccionesCliente";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        public string Post(ClienteDireccion Cd)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+                string query = @"
+                                INSERT INTO DireccionesCliente (IdCliente, Calle, Colonia, CP, Ciudad, Estado, NumeroInterior, NumeroExterior)
+                                VALUES (" + Cd.IdCliente + " , '" + Cd.Calle + "', '" + Cd.Colonia + "', '" + Cd.CP + "', '" + Cd.Ciudad + "', '" + Cd.Estado + "', '" + Cd.NumeroInterior + "', '" + Cd.NumeroExterior + "')" +
+                                ";";
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+
+
+                return "Direccion Cliente Agregado";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+            }
+        }
+
+        public string Put(ClienteDireccion dc)
+        {
+            try
+            {
+
+                DataTable table = new DataTable();
+
+                string query = @"
+                                update DireccionesCliente set IdCliente = " + dc.IdCliente + ", Calle = '" + dc.Calle + "' , Colonia = '" + dc.Colonia + "' , CP = '" + dc.CP + 
+                                "' , Ciudad = '" + dc.Ciudad + "' , Estado = '" + dc.Estado + "' , NumeroInterior = '" + dc.NumeroInterior + "' , NumeroExterior = '" + dc.NumeroExterior + 
+                                "' where IdDireccion = " + dc.IdDireccion + ";";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Se Actualizo Correctamente";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+
+                DataTable table = new DataTable();
+
+
+                string query = @"
+                              Delete DireccionesCliente where IdDireccion = " + id + ";";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Se Elimino Correctamente";
+            }
+            catch (Exception)
+            {
+                return "Se produjo un error";
+            }
+        }
+
+        //Obtener Direcciones por IdCliente
+        [Route("DireccionIdCliente/{id}")]
+        public HttpResponseMessage GetDireccionIdCliente(int id)
+        {
+            DataTable table = new DataTable();
+
+            string query = @"Select * from DireccionesCliente where IdCliente = " + id + ";";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+
+
+    }
+}
---
+++ Models/ClienteDireccion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProlappApi.Models
+{
+    public class ClienteDireccion
+    {
+        public long IdDireccion { get; set; }
+        public long IdCliente { get; set; }
+        public string Calle { get; set; }
+        public string Colonia { get; set; }
+        public string CP { get; set; }
+        public string Ciudad { get; set; }
+        public string Estado { get; set; }
+        public string NumeroInterior { get; set; }
+        public string NumeroExterior { get; set; }
+
+    }
+}
