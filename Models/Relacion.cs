c05dae18a8e765c6fdeb8915bfd45865f25ef775
RiztekIT <javier.gutierrez@riztek.com.mx>
2020-03-04 11:58:50 -0700

    Relacion

---
+++ Models/Relacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProlappApi.Models
+{
+    public class Relacion
+    {
+        public long IdRelacion { get; set; }
+        public string UUID { get; set; }
+        public string Tipo { get; set; }
+    }
+}
