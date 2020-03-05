9852a91fdf5d89c5a71c15a27aa7b1e7eef17f66
ivantalamantes <ivan.talamantes@live.com>
2020-03-05 12:46:35 -0700

    ...

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -32,6 +32,24 @@ namespace ProlappApi.Controllers
 
             return Request.CreateResponse(HttpStatusCode.OK, table);
         }
+        [Route("Facturar")]
+        public HttpResponseMessage GetFacturar()
+        {
+            DataTable table = new DataTable();
+
+            string query = @"select * from Cliente where IdApi <> '' and Estatus ='Activo' order by Nombre";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
         [Route("Id/{id}")]
         public HttpResponseMessage GetCliente(int id)
         {
