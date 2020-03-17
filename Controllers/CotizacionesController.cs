92c35d6e69bb5d2a71aff3e7958a4952bca28775
RiztekIT <javier.gutierrez@riztek.com.mx>
2020-03-17 14:58:55 -0700

    Cotizaciones

---
+++ Controllers/CotizacionesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProlappApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProlappApi.Controllers
+{
+    [RoutePrefix("api/Cotizaciones")]
+    public class CotizacionesController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            string query = @"exec stSelectTablaCotizaciones";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        [Route("BorrarCotizacion/{id}")]
+        public string Delete(int id)
+        {
+            try
+            {
+
+                DataTable table = new DataTable();
+
+                string query = @" exec dtBorrarCotizacion " + id;
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Se elimino Correctamente";
+            }
+            catch (Exception)
+            {
+                return "Error al Eliminar";
+            }
+        }
+
+        public string Put(Cotizaciones cotizaciones)
+        {
+            try
+            {
+
+
+                DataTable table = new DataTable();
+                DateTime time = cotizaciones.FechaDeExpedicion;
+                string format = "yyyy-MM-dd HH:mm:ss";
+
+                string query = @"
+                                exec etEditarCotizacion " + cotizaciones.IdCotizaciones + " , " +cotizaciones.IdCliente+ " , '" +cotizaciones.Nombre+ "' , '" +cotizaciones.RFC+ "' , '" +cotizaciones.Subtotal+"' , '" +cotizaciones.Total+ "' , '" +cotizaciones.Descuento+ "' , '"
+                                +cotizaciones.SubtotalDlls+ "' , '" +cotizaciones.TotalDlls+ "' , '" +cotizaciones.DescuentoDlls+ "', '" +cotizaciones.Observaciones+ "' , '" +cotizaciones.Vendedor+ "' , '" +cotizaciones.Moneda+ "' , '" +time.ToString(format)+ "' , '" 
+                                +cotizaciones.Flete+ "' , " +cotizaciones.Folio+ " , '" +cotizaciones.Telefono+ "' , '" +cotizaciones.Correo+ "' ";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Se Actualizo Correctamente";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+
+            }
+        }
+
+        public string Post(Cotizaciones cotizaciones)
+        {
+            try
+            {
+
+
+                DataTable table = new DataTable();
+                DateTime time = cotizaciones.FechaDeExpedicion;
+                string format = "yyyy-MM-dd HH:mm:ss";
+
+                string query = @" Execute itInsertNuevaCotizacion " + cotizaciones.IdCliente + " , '" + cotizaciones.Nombre + "' , '" + cotizaciones.RFC + "' , '" + cotizaciones.Subtotal + "' , '" + cotizaciones.Total + "' , '" + cotizaciones.Descuento + "' , '"
+                                + cotizaciones.SubtotalDlls + "' , '" + cotizaciones.TotalDlls + "' , '" + cotizaciones.DescuentoDlls + "' , '" + cotizaciones.Observaciones + "' , '" + cotizaciones.Vendedor + "' , '" + cotizaciones.Moneda + "' , '" + time.ToString(format) +
+                                "' , '" + cotizaciones.Flete + "' , " + cotizaciones.Folio + " , '" + cotizaciones.Telefono + "' , '" + cotizaciones.Correo + "'";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+
+
+                return "Cotizacion Agregada";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+            }
+        }
+
+    }
+}
---
+++ Models/Cotizaciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProlappApi.Models
+{
+    public class Cotizaciones
+    {
+        public long IdCotizaciones { get; set; }
+        public long IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string RFC { get; set; }
+        public string Subtotal { get; set; }
+        public string Total { get; set; }
+        public string Descuento { get; set; }
+        public string SubtotalDlls { get; set; }
+        public string TotalDlls { get; set; }
+        public string DescuentoDlls { get; set; }
+        public string Observaciones { get; set; }
+        public string Vendedor { get; set; }
+        public string Moneda { get; set; }
+        public DateTime FechaDeExpedicion { get; set; }
+        public string Flete { get; set; }
+        public long Folio { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+    }
+}
