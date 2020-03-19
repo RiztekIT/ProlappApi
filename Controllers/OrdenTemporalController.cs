2d58040116926aa5303e848eef5afd43733340ee
sierra_javier <javier.sierra@riztek.com.mx>
2020-03-19 16:03:37 -0700

    Orden Temporal

---
+++ Controllers/OrdenTemporalController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ProlappApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProlappApi.Controllers
+{
+    [RoutePrefix("api/OrdenTemporal")]
+    public class OrdenTemporalController : ApiController
+    {
+
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            string query = @"select * from dbo.OrdenTemporal";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        [Route("BorrarOrdenTemporal/{id}")]
+        public string Delete(int id)
+        {
+            try
+            {
+
+                DataTable table = new DataTable();
+
+                string query = @" exec dtBorrarOrdenTemporal " + id;
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Se elimino Correctamente";
+            }
+            catch (Exception)
+            {
+                return "Error al Eliminar";
+            }
+        }
+
+
+        public string Post(OrdenTemporal ot)
+        {
+            try
+            {
+
+                DataTable table = new DataTable();
+
+
+
+                string query = @"
+                                exec itInsertNuevaOrdenTemporal " + ot.IdTarima + " , " + ot.IdOrdenCarga + " , " + ot.IdOrdenDescarga +
+                                " , '" + ot.QR + "' , '" + ot.ClaveProducto + "' , '" + ot.Lote + "' , '" + ot.Sacos + @"'";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Se Actualizo Correctamente";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+
+            }
+        }
+
+        public string Put(OrdenTemporal ot)
+        {
+            try
+            {
+
+                DataTable table = new DataTable();
+                string query = @"
+                                exec itInsertNuevaOrdenTemporal " + ot.IdOrdenTemporal + " , " + ot.IdTarima + " , " + ot.IdOrdenCarga + " , " + ot.IdOrdenDescarga +
+                                " , '" + ot.QR + "' , '" + ot.ClaveProducto + "' , '" + ot.Lote + "' , '" + ot.Sacos + @"'";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Se Actualizo Correctamente";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+
+            }
+        }
+
+
+
+    }
+}
---
+++ Models/OrdenTemporal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProlappApi.Models
+{
+    public class OrdenTemporal
+    {
+
+        public long IdOrdenTemporal { get; set; }
+        public long IdTarima { get; set; }
+        public long IdOrdenCarga { get; set; }
+        public long IdOrdenDescarga { get; set; }
+        public string QR { get; set; }
+        public string ClaveProducto { get; set; }
+        public string Lote { get; set; }
+        public string Sacos { get; set; }
+
+    }
+}
