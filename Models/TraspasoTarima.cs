55fae2c959f16a3ceb5e0ab7d95a717f1c1bd467
sierra_javier <javier.sierra@riztek.com.mx>
2020-03-30 11:13:56 -0700

    Traspaso Tarima

--- Models/TraspasoTarima.cs
+++ Models/TraspasoTarima.cs
@@ -7,11 +7,16 @@ namespace ProlappApi.Models
 {
     public class TraspasoTarima
     {
-
         public long IdTraspasoTarima { get; set; }
-        public string Cantidad { get; set; }
-        public string QRorigen { get; set; }
-        public string QRdestino { get; set; }
+        public long IdOrigenTarima { get; set; }
+        public long IdDestinoTarima { get; set; }
+        public string ClaveProducto { get; set; }
+        public string Producto { get; set; }
+        public string Lote { get; set; }
+        public string Sacos { get; set; }
+        public DateTime FechaTraspaso { get; set; }
+        public long IdUsuario { get; set; }
+        public string Usuario { get; set; }
 
     }
 }
