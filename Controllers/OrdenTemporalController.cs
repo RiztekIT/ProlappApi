0a57cadeae08dae5d736dec769e18f0573a3d3ac
Rcdelarosa <ricardo.delarosa@riztek.com.mx>
2020-04-09 12:16:14 -0600

    ordentemporalIDOC

--- Controllers/OrdenTemporalController.cs
+++ Controllers/OrdenTemporalController.cs
@@ -160,6 +160,24 @@ namespace ProlappApi.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, table);
         }
 
+        //Obtener Orden Temporal por OrdenDescargaID
+        [Route("OrdenTemporalIDOD/{id}")]
+        public HttpResponseMessage OrdenTemporalIDOD(int id)
+        {
+            DataTable table = new DataTable();
+
+            string query = @"select * from OrdenTemporal where IdOrdenDescarga  =" + id + ";";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
 
 
     }
