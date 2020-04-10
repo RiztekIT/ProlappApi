e38bea6ec4c0a5c3e9b19e28f51f697620a73944
RiztekIT <javier.gutierrez@riztek.com.mx>
2020-04-10 14:47:49 -0600

    Prospectos

---
+++ Models/Prospecto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProlappApi.Models
+{
+    public class Prospecto
+    {
+        public long IdProspecto { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Empresa { get; set; }
+    }
+}
