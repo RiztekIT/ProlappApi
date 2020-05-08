05cbffdab1d3f6d37c96b32cee57232b6e0e4154
sierra_javier <javier.sierra@riztek.com.mx>
2020-05-08 15:49:22 -0600

    imagenes controler/model

---
+++ Controllers/ImagenesController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ProlappApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProlappApi.Controllers
+{
+    [RoutePrefix("api/Imagenes")]
+    public class ImagenesController : ApiController
+    {
+
+
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            string query = @"select * from Imagenes";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        [Route("GetImagenId/{id}")]
+        public HttpResponseMessage GetImagenId(int id)
+        {
+            DataTable table = new DataTable();
+
+            string query = @"select * from Imagenes where IdImagen = "+id;
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        [Route("GetImagenPath/{path}")]
+        public HttpResponseMessage GetImagenPath(string path)
+        {
+            DataTable table = new DataTable();
+
+            string query = @"select * from Imagenes where path = '" + path + "';";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+
+        [Route("BorrarImagen/{id}")]
+        public string Delete(int id)
+        {
+            try
+            {
+
+                DataTable table = new DataTable();
+
+                string query = @" exec dtBorrarImagen " + id;
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Se elimino Correctamente";
+            }
+            catch (Exception)
+            {
+                return "Error al Eliminar";
+            }
+        }
+
+        public string Post(Imagenes i)
+        {
+            try
+            {
+
+                DataTable table = new DataTable();
+
+                string query = @"
+                                exec itInsertNuevaImagen " + i.Folio + " , '" + i.Tipo + "' , '" + i.Imagen + "' , '" + i.Path + @"'";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Se Agrego Correctamente";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+
+            }
+        }
+
+        public string Put(Imagenes i)
+        {
+            try
+            {
+
+                DataTable table = new DataTable();
+
+                string query = @"
+                                exec etEditarImagen " + i.IdImagen + " , " + i.Folio + " , '" + i.Tipo + "' , '" + i.Imagen + "' , '" + i.Path + @"'";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Se Agrego Correctamente";
+            }
+            catch (Exception exe)
+            {
+                return "Se produjo un error" + exe;
+
+            }
+        }
+
+    }
+}
---
+++ Models/Imagen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProlappApi.Models
+{
+    public class Imagenes
+    {
+
+        public long IdImagen {get; set;}
+        public long Folio { get; set; }
+        public string Tipo { get; set; }
+        public string Imagen { get; set; }
+        public string Path { get; set; }
+    }
+}
