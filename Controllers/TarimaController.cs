c7d3b85405b04a29a5b6f88051036d5f7c780d9b
Rcdelarosa <ricardo.delarosa@riztek.com.mx>
2020-06-09 15:58:48 -0600

    Joins TarimaDtt/ODOT

--- Controllers/TarimaController.cs
+++ Controllers/TarimaController.cs
@@ -405,6 +405,25 @@ namespace ProlappApi.Controllers
 
             return Request.CreateResponse(HttpStatusCode.OK, table);
         }
+               
+        [Route("GetTarimaDttqr/{qr}")]
+        public HttpResponseMessage GetTarimaDttqr(string qr)
+        {
+            DataTable table = new DataTable();
+
+            string query = @"exec jnTarimaDtt" + qr;
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Prolapp"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
 
 
 
