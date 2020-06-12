b53e82d083719743e49237f9fc74da680665b4e3
sierra_javier <javier.sierra@riztek.com.mx>
2020-06-12 16:00:20 -0600

    .

--- Models/DetalleCompra.cs
+++ Models/DetalleCompra.cs
@@ -14,7 +14,6 @@ namespace ProlappApi.Models
         public string Cantidad { get; set; }
         public string PrecioUnitario { get; set; }
         public string CostoTotal { get; set; }
-        public string IVA { get; set; }
 
 
     }
