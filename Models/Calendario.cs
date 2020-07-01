ce5b358720ab0f00628621865f6fef8cb10991f5
sierra_javier <javier.sierra@riztek.com.mx>
2020-07-01 16:42:55 -0600

    Calencario

---
+++ Models/Calendario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProlappApi.Models
+{
+    public class Calendario
+    {
+       public int IdCalendario { set; get; }
+        public string NombreCalendario { set; get; }
+        public string Modulo { set; get; }
+        public string NombreUsuario { set; get; }
+    }
+}
