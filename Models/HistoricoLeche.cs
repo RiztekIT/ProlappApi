b0e550f1ce344c43125040230ec90a82db722b34
Rcdelarosa <ricardo.delarosa@riztek.com.mx>
2020-07-06 16:05:50 -0600

    error 2 (no dejaba mandar los dos al mismo tiempo)

---
+++ Models/HistoricoLeche.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProlappApi.Models
+{
+    public class HistoricoLeche
+    {
+        public long IdPrecio { get; set; }
+        public string PrecioLeche { get; set; }
+        public string VarianteDiaAnterior { get; set; }
+        public DateTime FechaPrecio { get; set; }
+    }
+}
