861099b21413863ee0dfb1647ea73163049e0612
Rcdelarosa <ricardo.delarosa@riztek.com.mx>
2020-08-07 13:34:28 -0600

    Fleteras

---
+++ Models/Fleteras.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProlappApi.Models
+{
+    public class Fleteras
+    {
+        public long IdFletera { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+    }
+}
