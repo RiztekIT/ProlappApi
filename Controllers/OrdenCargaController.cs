b5e132d941093149a6481e63f02e1ee100d00f8a
Sierra-Javier <56609008+Sierra-Javier@users.noreply.github.com>
2021-01-21 19:55:57 -0700

    Update OrdenCargaController.cs

--- Controllers/OrdenCargaController.cs
+++ Controllers/OrdenCargaController.cs
@@ -382,7 +382,7 @@ namespace ProlappApi.Controllers
         }
 
 
-        [Route("UpdateSaldo/{id}/{{saldo}}")]
+        [Route("UpdateSaldo/{id}/{saldo}")]
         public string PutUpdateSaldo(int id, string saldo)
 
         {
